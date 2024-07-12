62b080cab2590d120cefce7073494bbd8057ee26
Sam Ervin <servin2@theark.cloud>
2024-07-12 18:06:57 -0400

    Data and API work

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import pandas as pd
+import numpy as np
 from flask import Flask, request, jsonify, render_template
+
 
 app = Flask(__name__)   # create a Flask app
 
@@ -8,7 +11,10 @@ def Home():
 
 @app.route("/api/v1/<station>/<date>")
 def GetTempStationByDate(station, date):
-    temperature = 92
+    station_path = "data/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(station_path, skiprows=20, parse_dates=["    DATE"])    
+    temperature = df.loc[df['    DATE'] ==  str(date)] ['   TG'].squeeze() /10
+    
     return {"Station": station, "Date": date, "Temperature": temperature}
 
 if __name__ == "__main__":  # on running python app.py
